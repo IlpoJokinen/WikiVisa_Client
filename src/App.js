4a4c38b2b58996573f23cf033cb17b8e0af0a8d2
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-02-06 17:48:48 +0200

    a message sent to server and emitted to other clients

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:3001')
 
 function App() {
+  useEffect(() => {
+    socket.on('someoneClicked', (data) => {
+      console.log(data)
+    })
+
+    socket.on('senderConfirmation', (data) => {
+      console.log(data)
+    })
+  })
+
+  const pingConnectedNodes = (event) => {
+    event.preventDefault()
+    const message = 'Someone clicked...'
+    socket.emit('click', message)
+  }
+
   return (
     <div className="App">
-      <h3>OOO</h3>
+      <h3>Send a message to connected nodes</h3>
+      <form onSubmit={pingConnectedNodes}>
+        <input type="submit" value="Ping all nodes" />
+      </form>
     </div>
   );
 }
