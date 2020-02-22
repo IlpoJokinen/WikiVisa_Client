cad5311cd091d8cee4c88174a696b4f4950b21c7
Jan Lindlöf <janski.lindlof@gmail.com>
2020-02-22 14:17:32 +0200

    Generate random gamertag

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import PageTitle from './UI/PageTitle'
 import { withRouter } from "react-router-dom"
@@ -6,20 +6,26 @@ import { withRouter } from "react-router-dom"
 const WelcomeScreen = ({joinGame, history}) => {
 
     const [gamertag, setGamertag] = useState("")
+    
 
     const onChange = (e) => {
         e.preventDefault()
         setGamertag(e.target.value)
     }
 
+    //create guest + random 4 digit Integer gamertag
+    useEffect(() => {
+        const tag = 'guest'+Math.floor(1000 + Math.random() * 9000);
+        setGamertag(tag);
+
+      }, []);
+
     const onSubmit = (e) => {
         e.preventDefault()
         if(gamertag.length){
             joinGame({gamertag})
             history.push("/start")
-        } else {
-            window.alert("Please enter gamertag")
-        }
+        } 
         
     }
 
