745f9dff6bee9d05426689074af847d5a0bcdf00
Jan Lindlöf <janski.lindlof@gmail.com>
2020-03-13 07:00:47 +0200

    Added id:s to WelcomeScreen elements

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -17,18 +17,18 @@ const WelcomeScreen = ({ joiningState, joinGame, createGame, creatingState }) =>
             <Col>
                 <Form>
                     <Form.Group>
-                        <Form.Label>Enter room code <sup>*</sup></Form.Label>
-                        <Form.Control disabled={joiningState} size="lg" placeholder="Enter room code" onChange={(e) => setRoomCode(e.target.value)}/>
+                        <Form.Label id='roomcodelabel'>Enter room code <sup>*</sup></Form.Label>
+                        <Form.Control id='roomcode' disabled={joiningState} size="lg" placeholder="Enter room code" onChange={(e) => setRoomCode(e.target.value)}/>
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Enter your gamertag</Form.Label>
-                        <Form.Control disabled={joiningState} size="lg" placeholder="Enter your gamertag" onChange={(e) => setGamertag(e.target.value)}/>
+                        <Form.Label id='gamertaglabel'>Enter your gamertag</Form.Label>
+                        <Form.Control id='gamertag' disabled={joiningState} size="lg" placeholder="Enter your gamertag" onChange={(e) => setGamertag(e.target.value)}/>
                     </Form.Group>
-                    <Button style={{marginRight: 16}}variant={joiningState ? "secondary" : "primary"} onClick={() => joinGame(gamertag, roomCode)} type="button">
+                    <Button id='joingamebutton' style={{marginRight: 16}}variant={joiningState ? "secondary" : "primary"} onClick={() => joinGame(gamertag, roomCode)} type="button">
                         { joiningState ? <ArrowClockwise size={20} className="spin" /> : <PlayFill size={22} /> }
                         { joiningState ? " Joining game..." : " Join Game" }
                     </Button>
-                    <Button variant={creatingState ? "secondary" : "primary"} onClick={() => createGame(gamertag, roomCode)} type="button">
+                    <Button id='creategamebutton' variant={creatingState ? "secondary" : "primary"} onClick={() => createGame(gamertag, roomCode)} type="button">
                         { creatingState ? <ArrowClockwise size={20} className="spin" /> : <PlayFill size={22} /> }
                         { creatingState ? " Creating game..." : " Create Game" }
                     </Button>
