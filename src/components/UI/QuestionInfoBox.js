c3cdf0c92ead0b9b5a0d691003847dc920041323
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-05-08 06:04:27 +0300

    Size of questioncount indicator in questionScreen smaller

--- src/components/UI/QuestionInfoBox.js
+++ src/components/UI/QuestionInfoBox.js
@@ -8,7 +8,7 @@ const QuestionInfoBox = ({ question, questionCount, timeRemaining, questionIndex
     return (
         <Box style={{textAlign: "center"}}>
             <Box m={2}><Timer color={"#fff"} timeRemaining={timeRemaining}/></Box>
-            <Box m={2}><Header white size={5}>{questionIndex}/{questionCount}</Header></Box>
+            <Box m={2}><Header white size={6}>{questionIndex}/{questionCount}</Header></Box>
             <Box m={2}><Header white size={3}>{question}</Header></Box>
         </Box>
     )
