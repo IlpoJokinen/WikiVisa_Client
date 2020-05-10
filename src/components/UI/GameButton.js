bb5a50070b4952ba7244fbfa1061b9a264791fb2
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-05-11 02:39:06 +0300

    removed gamebutton, removed unused dependencies

--- src/components/UI/GameButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
- 
-const GameButton = (props) => {
-    let { id, title, onClickFunc } = props
-    return <button id={id} onClick={onClickFunc} className={props.hasOwnProperty("className") ? props.className: "gameButton"}>{ title }</button>
-}
- 
-export default GameButton
